dc0b05c24002b934e459033bc3b3fd4723d497d5
when <44486276+MzzdToT@users.noreply.github.com>
2023-09-07 10:49:36 +0800

    Update mysql.go

--- Plugins/mysql.go
+++ Plugins/mysql.go
@@ -1,26 +1,41 @@
 package Plugins
 
 import (
-    "errors"
+    "database/sql"
     "fmt"
+    "io/ioutil"
+    "strings"
+    _ "github.com/go-sql-driver/mysql"
+    
 )
 
 // MysqlScan 尝试使用提供的用户名和密码爆破 MySQL 数据库
-func MysqlScan(ip string ,user string, pass string, dbPort int) error {
-    // 在这里进行一些检查，如果发现错误情况，就返回一个错误
-    if user == "" {
-        return errors.New("用户名不能为空")
+func MysqlScan(ip string, user string, pass string, dbPort int) error {
+    fmt.Println("ok")
+    // 构建 MySQL 连接字符串
+    dataSourceName := fmt.Sprintf("%s:%s@tcp(%s:%d)/mysql", user, pass,ip, dbPort)
+    fmt.Println(dataSourceName)
+    if err := MysqlConn(dataSourceName); err == nil {
+        fmt.Printf("爆破成功! ：%s:%d %s:%s\n",ip ,dbPort , user, pass)
+        return nil
+    } else {
+        fmt.Printf("正在测试：%s:%d %s:%s", ip,dbPort , user, pass)
+        return fmt.Errorf("连接失败: %v", err)
+    }
+}
+
+
+// MysqlConn 尝试建立 MySQL 数据库连接
+func MysqlConn(dataSourceName string) error {
+    db, err := sql.Open("mysql", dataSourceName)
+    if err != nil {
+        return err
+    }
+    defer db.Close()
+
+    if err := db.Ping(); err != nil {
+        return err
     }
 
-    if pass == "" {
-        return errors.New("密码不能为空")
-    }
-
-    if dbPort <= 0 {
-        return errors.New("无效的端口号")
-    }
-
-    // 如果一切正常，返回 nil，表示没有错误
-    fmt.Println("%s:%d user:%s pass:%s ",ip,dbPort,user,pass)
     return nil
 }
