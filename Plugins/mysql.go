e5904f13f449503b425e58f13168be5fa88e43a9
when <44486276+MzzdToT@users.noreply.github.com>
2023-09-08 19:50:32 +0800

    Update mysql.go

--- Plugins/mysql.go
+++ Plugins/mysql.go
@@ -3,24 +3,23 @@ package Plugins
 import (
     "database/sql"
     "fmt"
-    "io/ioutil"
-    "strings"
+
     _ "github.com/go-sql-driver/mysql"
     
 )
 
+
 // MysqlScan 尝试使用提供的用户名和密码爆破 MySQL 数据库
 func MysqlScan(ip string, user string, pass string, dbPort int) error {
-    fmt.Println("ok")
+
     // 构建 MySQL 连接字符串
-    dataSourceName := fmt.Sprintf("%s:%s@tcp(%s:%d)/mysql", user, pass,ip, dbPort)
-    fmt.Println(dataSourceName)
+    dataSourceName := fmt.Sprintf("%s:%s@tcp(%v:%v)/mysql?charset=utf8", user, pass, ip, dbPort)
+    fmt.Println(user,pass,dataSourceName)
     if err := MysqlConn(dataSourceName); err == nil {
-        fmt.Printf("爆破成功! ：%s:%d %s:%s\n",ip ,dbPort , user, pass)
+        fmt.Printf("爆破成功! ：%v:%v %v:%v\n",ip ,dbPort , user, pass)
         return nil
     } else {
-        fmt.Printf("正在测试：%s:%d %s:%s", ip,dbPort , user, pass)
-        return fmt.Errorf("连接失败: %v", err)
+        return err
     }
 }
 
@@ -39,3 +38,4 @@ func MysqlConn(dataSourceName string) error {
 
     return nil
 }
+
